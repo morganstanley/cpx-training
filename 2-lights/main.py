4984c7a1873e1a0ba306bc1ec901803fc6af88f1
Mimi Flynn <mimiflynn@gmail.com>
2019-01-19 10:28:37 -0500

    exercise 2 and light meter

---
+++ 2-lights/main.py
@@ -0,0 +1,19 @@
+# CircuitPlaygroundExpress_NeoPixel
+import board
+import neopixel
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.1)
+pixels.fill((0, 0, 0))
+
+while True:
+    pixels[0] = (255, 0, 0)
+    pixels[1] = (255, 127, 0)
+    pixels[2] = (0, 255, 0)
+    pixels[3] = (0, 255, 127)
+    pixels[4] = (75, 0, 130)
+    pixels[5] = (60, 0, 255)
+    pixels[6] = (255, 255, 255)
+    pixels[7] = (0, 100, 0)
+    pixels[8] = (100, 0, 0)
+    pixels[9] = (0, 0, 100)
+    pixels.show()
