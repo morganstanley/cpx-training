afa42713163d496e83988b6aa6351ef3741070d6
Mimi Flynn <mimiflynn@gmail.com>
2019-03-30 11:52:14 -0400

    variables and functions WIP

--- Level 2 - Intro to Programming/1-variables/main.py
+++ Level 2 - Intro to Programming/1-variables/main.py
@@ -1,5 +1,17 @@
 # Learn more https://realpython.com/python-variables/
 # and https://realpython.com/python-data-types/
+import time
+from adafruit_circuitplayground.express import cpx
 
+pixels = cpx.pixels
+cpx.pixels.brightness = 0.3
+pixels.fill((0, 0, 0))
+pixels.show()
 
+RED = (255, 0, 0)
 
+while True:
+    for i in range(len(pixels)):
+        pixels[i] = RED
+        time.sleep(.05)
+    time.sleep()
