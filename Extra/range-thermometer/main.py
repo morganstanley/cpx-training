f6e9e38a923f268be4995244212c6afe1fc092b2
Mimi Flynn <mimiflynn@gmail.com>
2020-03-22 12:34:26 -0400

    temp color range

--- Extra/range-thermometer/main.py
+++ Extra/range-thermometer/main.py
@@ -1,3 +1,6 @@
+# Circuit Playground Temperature
+# Reads the on-board temperature sensor and prints the value
+
 import time
 import adafruit_thermistor
 import board
@@ -13,19 +16,43 @@ pixels.show()
 thermistor = adafruit_thermistor.Thermistor(
     board.TEMPERATURE, 10000, 10000, 25, 3950)
 
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+PINK = (255, 100, 120)
+ORANGE = (255, 100, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+PURPLE = (255, 0, 255)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (80, 200, 175)
+
+def set_pixels():
+    pixels[0] = PURPLE
+    pixels[1] = BLUE
+    pixels[2] = BLUE
+    pixels[3] = CYAN
+    pixels[4] = LIGHT_BLUE
+    pixels[5] = YELLOW
+    pixels[6] = ORANGE
+    pixels[7] = RED
+    pixels[8] = RED
+    pixels[9] = RED
+
 while True:
     temp_c = thermistor.temperature
     temp_f = thermistor.temperature * 9 / 5 + 32
     print("Temperature is: %f C and %f F" % (temp_c, temp_f))
 
+    set_pixels()
+
     # light value remapped to pixel position
     peak = simpleio.map_range(temp_f, 0, 100, 0, 9)
 
-    for i in range(0, 9, 1):
-        if i <= peak:
-            pixels[i] = (255, 0, 0)
-        else:
+    for i in range(0, 10, 1):
+        if i >= peak:
             pixels[i] = (0, 0, 0)
+
     pixels.show()
 
     time.sleep(0.25)
