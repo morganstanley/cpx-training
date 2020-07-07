686ec7e95a10f8e7acd428a4684efbbcb495b58f
Mimi <mimiflynn@gmail.com>
2020-07-07 18:58:18 -0400

    clean up extras

--- code/Extra/4-touch/main.py
+++ code/Extra/4-touch/main.py
@@ -1,13 +1,32 @@
-from adafruit_circuitplayground.express import cpx
+# CircuitPlaygroundExpress_CapTouch
+
 import time
 
-cpx.pixels.brightness = 0.3
+import board
+import touchio
 
-while True: 
-    if cpx.touch_A1:
-        cpx.pixels.fill( (255, 0, 0) )
-        cpx.pixels.show()
-    if cpx.touch_A7:
-        cpx.pixels.fill( (0, 0, 0) )
-        cpx.pixels.show()
+touch1 = touchio.TouchIn(board.A1)
+touch2 = touchio.TouchIn(board.A2)
+touch3 = touchio.TouchIn(board.A3)
+touch4 = touchio.TouchIn(board.A4)
+touch5 = touchio.TouchIn(board.A5)
+touch6 = touchio.TouchIn(board.A6)
+touch7 = touchio.TouchIn(board.A7)
+
+while True:
+    if touch1.value:
+        print("A1 touched!")
+    if touch2.value:
+        print("A2 touched!")
+    if touch3.value:
+        print("A3 touched!")
+    if touch4.value:
+        print("A4 touched!")
+    if touch5.value:
+        print("A5 touched!")
+    if touch6.value:
+        print("A6 touched!")
+    if touch7.value:
+        print("A7 touched!")
+
     time.sleep(0.01)
