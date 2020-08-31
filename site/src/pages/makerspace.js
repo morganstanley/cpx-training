ba9883a8cfeee098f080d658da3440182fa30c7d
Mimi Flynn <mimiflynn@gmail.com>
2020-08-31 17:30:30 -0400

    make a makerspace workshop intro content

--- site/src/pages/makerspace.js
+++ site/src/pages/makerspace.js
@@ -15,19 +15,39 @@ const MakerspacePage = ({ data, location }) => {
           <div className="pane">
             <header className="content">
               <h2>
-                <span>Make a Makerspace</span>
+                <span>Maker Magic</span>
               </h2>
             </header>
+            <section className="content">
+              <span>
+                Design Your Own Blueprint for Community Tech Empowerment
+              </span>
+            </section>
           </div>
         </article>
-        <SEO title="About" />
+        <SEO title="Make a Makerspace" />
         <article className="content">
-          <h2>Mission</h2>
-          <p>The Morgan Stanley Makerspace works towards two of the Firm's core values: Give back and diversity and inclusion.</p>
-          <h3>Give Back</h3>
-          <p></p>
-          <h3>Commit to Diversity and Inclusion</h3>
-          <p>More details coming soon.</p>
+          <h2>Motivation</h2>
+          <p>In order to fill the tech- and schience-focused roles of the future, technologists nationwide should focus on providing youth from underserved communities the resources to promote computer science advocacy now.</p>
+          <p>Students from uderprivileged backgrounds are 8 to 10 times more likely to pursue college degrees in computer science if they have taken AP computer science in high school. We can project that these odds will only increase if studentds are given even earlier exposure to software and hardware technologies.</p>
+          <p>Through this Maker workshop, technologists who are passionate about bridging this gap will develop a blueprint that can be leveraged to facilitate tech exploration workshops for underserved youth in their communities. Marrying knowledge of Python programming and Adafruit Circuit Playground Express devices, their students will be empowered to become makers of technology.</p>
+          <h3>Goals</h3>
+          <ul>
+            <li>Unveil the magic behind technology to young people with limited access to computer science education through the Adafruit CPX.</li>
+            <li>Draw best practices from a Morgan Stanley employee designed and run program.</li>
+            <li>Leave with a Makerspace workshop blueprint for youth in your community.</li>
+          </ul>
+          <h3>Audience</h3>
+          <p>This workshop caters to software and hardware enthusiasts. It is especially useful to thos who are excited about leveraging devices to teach coding and electronics to young programmers!</p>
+          <ul>
+            <li>College Students</li>
+            <li>Educators</li>
+            <li>Corporate Technologists</li>
+            <li>Start Up Technologists</li>
+            <li>Non Profit Organization Technologists</li>
+            <li>Hobbyists</li>
+            <li>Parents with children learning from home ;)</li>
+          </ul>
         </article>
       </div >
     </Layout >
