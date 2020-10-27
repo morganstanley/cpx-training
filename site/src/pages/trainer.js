22c9fcb199275deecc1c649cbda13e8d9775dc59
Craig Gosnay <craig.gosnay@gmail.com>
2020-10-27 10:33:12 -0400

    updating train the trainer section

--- site/src/pages/trainer.js
+++ site/src/pages/trainer.js
@@ -22,12 +22,21 @@ const TrainerPage = ({ data, location }) => {
         </article>
         <SEO title="About" />
         <article className="content">
-          <h2>Mission</h2>
-          <p>The Morgan Stanley Makerspace works towards two of the Firm's core values: Give back and diversity and inclusion.</p>
-          <h3>Give Back</h3>
-          <p></p>
-          <h3>Commit to Diversity and Inclusion</h3>
-          <p>More details coming soon.</p>
+          <h2>Mission & Philosophy</h2>
+          <p>Train the trainer sessions are a starting point for volunteers of all backgrounds 
+            to gather in a space and learn what goes into a Morgan Stanley Makerspace session. 
+            There will be a presentation of the curriculum, the opportunity to gain some hands 
+            on experience with the CPX device, and a discussion of the philosphy with which we 
+            run our makerspaces.</p>
+            <p/>
+            <p>The Morgan Stanley Makerspaces are designed to introduce participants to coding 
+              and making with the Adafruit CPX device as a starting platform.</p>
+            <p/>
+            <p>The philosophy is one of discovery, experimentation and passion. The curriculum 
+              is not used as a test or a required standard to achieved, but a jumping off point.</p>
+            <p/>
+            <p>We hope that by the end of the session, the participants will have cultivated a 
+              desire to delve deeper and learn more about whichever aspect of technology inspired them.</p>
         </article>
       </div >
     </Layout >
