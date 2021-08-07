b0624d1cb5b9f7e1c79c6c17283fbb9de9b1cd76
Mimi Flynn <mimiflynn@gmail.com>
2021-08-07 10:59:09 -0400

    instructor notes tips and tricks

--- site/src/pages/instructor.js
+++ site/src/pages/instructor.js
@@ -15,7 +15,7 @@ const InstructorPage = ({ data, location }) => {
                     <div className="pane">
                         <header className="content">
                             <h2>
-                                <span>Train the Trainer</span>
+                                <span>Instructor Notes</span>
                             </h2>
                         </header>
                     </div>
@@ -37,6 +37,24 @@ const InstructorPage = ({ data, location }) => {
                     <p />
                     <p>We hope that by the end of the session, the participants will have cultivated a
                         desire to delve deeper and learn more about whichever aspect of technology inspired them.</p>
+                </article>
+                <article className="content">
+                    <h2>Tips and Tricks from Experience Instructors</h2>
+                    <h3>General Guidance</h3>
+                    <ul>
+                        <li>Give students time and space to explore what catches their interest in each exercise.</li>
+                        <li>Feel free to make mistakes in front of them and say that you don't know something. Let them know you are learning too.</li>
+                        <li>When you don't know something, work through it out loud with the group. The troubleshooting process is a valuable skill to learn even if they don't take to coding.</li>
+                        <li>If your group shows an interest in a certain aspect of a lession (color theory, music notes, etc), dive into it! This is what we are hoping for! Don't stress about the number of lessons. This moment is the most valuable learning moment.</li>
+                        <li>If the lessons seem easy to your group, add challenges and encourage creativity.</li>
+                    </ul>
+                    <h3>Remote Lessons</h3>
+                    <ul>
+                        <li>Give students quiet time to work through the code. Check in by name in a rotating fashion.</li>
+                        <li>Share your screen and demonstrate examples with your webcam if possible.</li>
+                        <li>Encourage students to keep their camera on and mics unmuted to foster troubleshooting when needed and natural conversation.</li>
+                        <li>Encourage students to share their screen when troubleshooting.</li>
+                    </ul>
                 </article>
             </div >
         </Layout >
