e9b4372612dd750fc51dae2e6d75d9c60e7a9b62
Mimi Flynn <mimiflynn@gmail.com>
2021-08-27 11:56:35 -0400

    case for change design

--- site/src/pages/makerspace.js
+++ site/src/pages/makerspace.js
@@ -28,37 +28,33 @@ const MakerspacePage = ({ data, location }) => {
                 <SEO title="Make a Makerspace" />
                 <article className="content">
                     <h2>Motivation</h2>
-                    <blockquote>
-                        <b>Minority students in under-resourced  or underperforming  school districts are at risk of being left behind</b>, a scenario that
-                        has far-reaching socioeconomic consequences for the United States.
-                    </blockquote>
-                    <p>In order to fill the tech- and science-focused roles of the future, technologists nationwide should focus on providing youth from underserved communities the resources to promote computer science advocacy now.</p>
-                    <p>Students from underprivileged backgrounds are 8 to 10 times more likely to pursue college degrees in computer science if they have taken AP computer science in high school. We can project that these odds will only increase if studentds are given even earlier exposure to software and hardware technologies.</p>
-                    <p>Through this Maker workshop, technologists who are passionate about bridging this gap will develop a blueprint that can be leveraged to facilitate tech exploration workshops for underserved youth in their communities. Marrying knowledge of Python programming and Adafruit Circuit Playground Express devices, their students will be empowered to become makers of technology.</p>
+                    <div className="grid">
+                        <section className="column">
+                            <p>In order to fill the tech- and science-focused roles of the future, technologists nationwide should focus on providing youth from underserved communities the resources to promote computer science advocacy now.</p>
+                            <p>Students from underprivileged backgrounds are 8 to 10 times more likely to pursue college degrees in computer science if they have taken AP computer science in high school. We can project that these odds will only increase if studentds are given even earlier exposure to software and hardware technologies.</p>
+                            <p>Through this Maker workshop, technologists who are passionate about bridging this gap will develop a blueprint that can be leveraged to facilitate tech exploration workshops for underserved youth in their communities. Marrying knowledge of Python programming and Adafruit Circuit Playground Express devices, their students will be empowered to become makers of technology.</p>
+                        </section>
+                        <section className="column">
+                            <blockquote>
+                                <b>Minority students in under-resourced  or underperforming  school districts are at risk of being left behind</b>, a scenario that
+                                has far-reaching socioeconomic consequences for the United States.
+                            </blockquote>
+                        </section>
+                    </div>
                 </article>
                 <article className="content">
-                    <div className="talking-points">
-                        <header className="content">
-                            <h3>
-                                <span>
-                                    The Case for Change
-                                </span>
-                            </h3>
-                        </header>
-                        <div className="content">
-                            <div className="grid">
-                                <section className="column">
-                                    <h3>1.4m</h3>
-                                    <p className="highlights">Number of computer-science-related jobs available in 2021</p>
-                                    <h3>400,000</h3>
-                                    <p className="highlights">Number of graduates with the skills to apply for those jobs</p>
-                                </section>
-                                <section className="column">
-                                    <h3>8-10x</h3>
-                                    <p className="highlights">Increase Likelihood that students from underserved backgrounds will pursue degrees in computer science if they have taken computer science in high school</p>
-                                </section>
-                            </div>
-                        </div>
+                    <h2>The Case for Change</h2>
+                    <div className="grid">
+                        <section className="column">
+                            <h3>1.4m</h3>
+                            <p className="highlights">Number of computer-science-related jobs available in 2021</p>
+                            <h3>400,000</h3>
+                            <p className="highlights">Number of graduates with the skills to apply for those jobs</p>
+                        </section>
+                        <section className="column">
+                            <h3>8-10x</h3>
+                            <p className="highlights">Increase Likelihood that students from underserved backgrounds will pursue degrees in computer science if they have taken computer science in high school</p>
+                        </section>
                     </div>
                 </article>
                 <article className="content">
