5bc67a306ea9c880321f379a39f8a7f5d93009c8
Mimi Flynn <414934+mimiflynn@users.noreply.github.com>
2023-12-26 14:07:03 -0500

    table of contents and next previous links

--- site/src/templates/exercise.js
+++ site/src/templates/exercise.js
@@ -4,31 +4,79 @@ import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
+function nextPrev(nodes, location) {
+  const len = nodes.length;
+  const current = nodes.findIndex((node) =>
+    location.pathname.includes(node.fields.slug)
+  );
+  const prevNode = current !== 0 ? nodes[current - 1] : false;
+  const nextNode = current !== len - 1 ? nodes[current + 1] : false;
+  return [nextNode, prevNode];
+}
+
 const ExerciseTemplate = ({ children, data, pageContext, location }) => {
   const pageTitle = pageContext.frontmatter.title;
+  const toc = data.mdx.tableOfContents.items;
   const nodes = data.allMdx.nodes;
+  const [next, previous] = nextPrev(nodes, location);
 
   return (
     <Layout location={location}>
       <Seo title={pageTitle} description={pageContext.description} />
       <article className="exercise-main content">
-        <header>
-          <h2>{pageTitle}</h2>
-        </header>
-        {children}
+        <nav className="nav exercise-nav">
+          <ul>
+            {nodes.map((node) => {
+              const current = location.pathname.includes(node.fields.slug);
+              const title = node.frontmatter.title;
+              return (
+                <li className={current ? 'current' : ''}>
+                  <Link to={node.fields.slug}>
+                    {node.frontmatter.exercise} ) {title}
+                  </Link>
+                  {current && (
+                    <nav className="nav exercise-content-nav">
+                      <ul>
+                        {toc &&
+                          toc.map((item) => (
+                            <li>
+                              <Link to={item.url}>{item.title}</Link>
+                            </li>
+                          ))}
+                      </ul>
+                    </nav>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <div className="exercise-content">
+          <header>
+            <h2>{pageTitle}</h2>
+          </header>
+          {children}
+        </div>
       </article>
 
       <nav className="content">
-        <ul className="toc">
-          {nodes.map((node) => {
-            const current = location.pathname.includes(node.fields.slug);
-            const title = node.frontmatter.title;
-            return (
-              <li className={current ? 'current' : ''}>
-                <Link to={node.fields.slug}>{title}</Link>
-              </li>
-            );
-          })}
+        <ul className="exercise-nav">
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← Exercise {previous.frontmatter?.exercise} |{' '}
+                {previous.frontmatter?.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                Exercise {next.frontmatter?.exercise} |{' '}
+                {next.frontmatter?.title} →
+              </Link>
+            )}
+          </li>
         </ul>
       </nav>
     </Layout>
@@ -68,6 +116,7 @@ export const pageQuery = graphql`
         excerpt
         frontmatter {
           title
+          exercise
         }
         internal {
           contentFilePath
