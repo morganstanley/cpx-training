92437278a0d6f97ec1f9cac3fe735a591bffcfd6
Mimi Flynn <mimiflynn@gmail.com>
2025-01-21 14:34:39 -0500

    selected language to exercise template

---
+++ site/__mocks__/exercises.js
@@ -0,0 +1,124 @@
+export const languages = ['en-US', 'fr-CA'];
+
+export const slug = '/documentation/2.0/components/';
+
+export const allExercises = [
+  {
+    id: '105d960d-43f9-5c51-9367-5f01020cec2c',
+    tableOfContents: {
+      items: [
+        {
+          url: '#learn',
+          title: 'Learn',
+          items: [
+            {
+              url: '#multi-week-programs',
+              title: 'Multi-Week Programs',
+              items: [
+                {
+                  url: '#introduction-to-makecode',
+                  title: 'Introduction to MakeCode',
+                },
+                {
+                  url: '#introduction-to-circuitpython',
+                  title: 'Introduction to CircuitPython',
+                },
+                {
+                  url: '#robotics',
+                  title: 'Robotics',
+                },
+              ],
+            },
+            {
+              url: '#single-day-programs',
+              title: 'Single-Day Programs',
+              items: [
+                {
+                  url: '#intro-to-circuitpython---level-1',
+                  title: 'Intro to CircuitPython - Level 1',
+                },
+                {
+                  url: '#coding-with-circuitpython---level-2',
+                  title: 'Coding with CircuitPython - Level 2',
+                },
+                {
+                  url: '#imagicharm-lessons',
+                  title: 'ImagiCharm Lessons',
+                  items: [
+                    {
+                      url: '#colorful-pixels---level-1',
+                      title: 'Colorful Pixels - Level 1',
+                    },
+                    {
+                      url: '#moving-pixels---level-2',
+                      title: 'Moving Pixels - Level 2',
+                    },
+                    {
+                      url: '#rainbow-glow---level-3',
+                      title: 'Rainbow Glow - Level 3',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              url: '#get-started-today',
+              title: 'Get Started Today!',
+            },
+          ],
+        },
+      ],
+    },
+    frontmatter: {
+      template: null,
+      title: 'Learn',
+      tags: null,
+      level: null,
+      exercise: null,
+      category: null,
+      description: null,
+    },
+    internal: {
+      contentFilePath:
+        '/home/mimiflynn/Projects/cpx-training/site/content/exercises/en-US/index.mdx',
+    },
+    fields: {
+      slug: '/exercises/en-US/',
+    },
+  },
+  {
+    id: '13955014-01d7-5704-a740-96a897379a56',
+    tableOfContents: {
+      items: [
+        {
+          url: '#introduction-to-circuitpython---level-1',
+          title: 'Introduction to CircuitPython - Level 1',
+        },
+        {
+          url: '#programming-with-circuitpython---level-2',
+          title: 'Programming with CircuitPython - Level 2',
+        },
+        {
+          url: '#programming-with-circuitpython---robotics',
+          title: 'Programming with CircuitPython - Robotics',
+        },
+      ],
+    },
+    frontmatter: {
+      template: null,
+      title: 'CircuitPython',
+      tags: null,
+      level: null,
+      exercise: null,
+      category: 'CircuitPython',
+      description: null,
+    },
+    internal: {
+      contentFilePath:
+        '/home/mimiflynn/Projects/cpx-training/site/content/exercises/fr-CA/circuitpython/index.mdx',
+    },
+    fields: {
+      slug: '/exercises/en-US/circuitpython/',
+    },
+  },
+];
