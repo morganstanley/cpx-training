b8c0fbc401c2fca1d9c43e8fb802b89f9b30be2e
Mimi <mimi.flynn@morganstanley.com>
2025-01-22 12:11:09 -0500

    language logic and tests

--- site/src/utils/language.spec.js
+++ site/src/utils/language.spec.js
@@ -4,122 +4,28 @@ import { allExercises, slug, languages } from '../../__mocks__/exercises';
 test('Gets documentation by version', () => {
   expect(getLanguage(allExercises, 'en-US')).toEqual([
     {
-      id: '105d960d-43f9-5c51-9367-5f01020cec2c',
-      tableOfContents: {
-        items: [
-          {
-            url: '#learn',
-            title: 'Learn',
-            items: [
-              {
-                url: '#multi-week-programs',
-                title: 'Multi-Week Programs',
-                items: [
-                  {
-                    url: '#introduction-to-makecode',
-                    title: 'Introduction to MakeCode',
-                  },
-                  {
-                    url: '#introduction-to-circuitpython',
-                    title: 'Introduction to CircuitPython',
-                  },
-                  {
-                    url: '#robotics',
-                    title: 'Robotics',
-                  },
-                ],
-              },
-              {
-                url: '#single-day-programs',
-                title: 'Single-Day Programs',
-                items: [
-                  {
-                    url: '#intro-to-circuitpython---level-1',
-                    title: 'Intro to CircuitPython - Level 1',
-                  },
-                  {
-                    url: '#coding-with-circuitpython---level-2',
-                    title: 'Coding with CircuitPython - Level 2',
-                  },
-                  {
-                    url: '#imagicharm-lessons',
-                    title: 'ImagiCharm Lessons',
-                    items: [
-                      {
-                        url: '#colorful-pixels---level-1',
-                        title: 'Colorful Pixels - Level 1',
-                      },
-                      {
-                        url: '#moving-pixels---level-2',
-                        title: 'Moving Pixels - Level 2',
-                      },
-                      {
-                        url: '#rainbow-glow---level-3',
-                        title: 'Rainbow Glow - Level 3',
-                      },
-                    ],
-                  },
-                ],
-              },
-              {
-                url: '#get-started-today',
-                title: 'Get Started Today!',
-              },
-            ],
-          },
-        ],
-      },
-      frontmatter: {
-        template: null,
-        title: 'Learn',
-        tags: null,
-        level: null,
-        exercise: null,
-        category: null,
-        description: null,
-      },
-      internal: {
-        contentFilePath:
-          '/home/mimiflynn/Projects/cpx-training/site/content/exercises/en-US/index.mdx',
-      },
+      id: 'a9756c78-5cc2-5d50-9434-22fbd314e502',
       fields: {
-        slug: '/exercises/en-US/',
+        slug: '/exercises/en-US/circuitpython/',
+      },
+    },
+    {
+      id: '6698d395-fd02-5ca8-a485-0dd1ff1de0bc',
+      fields: {
+        slug: '/exercises/en-US/makecode/',
+      },
+    },
+    {
+      id: 'c7600476-3796-5367-af93-631c77005380',
+      fields: {
+        slug: '/exercises/en-US/circuitpython/setup_bluefruit/',
       },
     },
   ]);
 
-  expect(getLanguage(allExercises, 'en-US')).toEqual([
+  expect(getLanguage(allExercises, 'fr-CA')).toEqual([
     {
-      id: '13955014-01d7-5704-a740-96a897379a56',
-      tableOfContents: {
-        items: [
-          {
-            url: '#introduction-to-circuitpython---level-1',
-            title: 'Introduction to CircuitPython - Level 1',
-          },
-          {
-            url: '#programming-with-circuitpython---level-2',
-            title: 'Programming with CircuitPython - Level 2',
-          },
-          {
-            url: '#programming-with-circuitpython---robotics',
-            title: 'Programming with CircuitPython - Robotics',
-          },
-        ],
-      },
-      frontmatter: {
-        template: null,
-        title: 'CircuitPython',
-        tags: null,
-        level: null,
-        exercise: null,
-        category: 'CircuitPython',
-        description: null,
-      },
-      internal: {
-        contentFilePath:
-          '/home/mimiflynn/Projects/cpx-training/site/content/exercises/fr-CA/circuitpython/index.mdx',
-      },
+      id: '32212966-4d2e-5868-81ef-5a40324a2891',
       fields: {
         slug: '/exercises/fr-CA/circuitpython/',
       },
@@ -135,15 +41,15 @@ test('Gets language of current exercise', () => {
     'fr-CA'
   );
   expect(
-    getCurrentLanguage('/exercises/stuff/components/', [...versions, 'stuff'])
+    getCurrentLanguage('/exercises/stuff/components/', [...languages, 'stuff'])
   ).toEqual('stuff');
   expect(
     getCurrentLanguage('/exercises/3.4.1-rc3/components/', [
-      ...versions,
+      ...languages,
       '3.4.1-rc3',
     ])
   ).toEqual('3.4.1-rc3');
   expect(
-    getCurrentLanguage('/exercises/3.4.1-rc3/components/', versions)
+    getCurrentLanguage('/exercises/3.4.1-rc3/components/', languages)
   ).toEqual(undefined);
 });
